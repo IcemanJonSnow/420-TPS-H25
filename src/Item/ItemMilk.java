2411e2970de3ed0ab41150c0b80e2a3770887d34
Samuel <parasam31@gmail.com>
2025-06-02 16:11:24 -0400

    parametres et methodes de la classe ItemMilk.java

--- src/Item/ItemMilk.java
+++ src/Item/ItemMilk.java
@@ -1,4 +1,31 @@
 package Item.Item;
 
-public class ItemMilk {
+public class ItemMilk extends Item{
+
+    private double fat, liters;
+    
+    public ItemMilk(Category category, int id, String name, double price, double fat, double liters) {
+        super(category, id, name, price);
+        this.fat = fat;
+        this.liters = liters;
+    }
+    
+    
+    public Category getCategory() {
+        return Category.Milk;
+    }
+    
+    public String getCategoryString() {
+        return "Milk";
+    }
+    
+    public double getFat() {return fat;}
+    public void setFat(double fat){this.fat = fat;}
+
+    public double getLiters() {return liters;}
+    public void setLiters(double liters){this.liters = liters;}
+    
+    public String getInfoToString(){
+        return "";
+    }
 }
