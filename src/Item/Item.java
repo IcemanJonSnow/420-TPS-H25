a41a1451a22bf5670aa98d9b957fc0381be8152e
Samuel <parasam31@gmail.com>
2025-06-02 15:52:19 -0400

    parametres et methodes de la classe Item.java

--- src/Item/Item.java
+++ src/Item/Item.java
@@ -1,4 +1,50 @@
 package Item.Item;
 
-public class Item {
+public abstract class Item {
+
+    private Category category;
+    private int id;
+    private String name;
+    private double price;
+    private int quantityInStock;
+    
+    public Item(Category category, int id, String name, double price) {
+        this.category = category;
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.quantityInStock = 0;
+    }
+    
+    public abstract Category getCategory();
+    
+    public abstract String getCategoryString();
+    
+    public int getId() {return id;}
+    public void setId(int id) {this.id = id;}
+    
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+    
+    public double getPrice() {return price;}
+    public void setPrice(double price) {this.price = price;}
+    
+    public int getQuantityInStock() {return quantityInStock;}
+    public void setQuantityInStock(int quantityInStock) {this.quantityInStock = quantityInStock;}
+    
+    public void increaseQuantityInStock(int quantityInStock) {
+        this.quantityInStock += quantityInStock;
+    }
+    
+    public void decreaseQuantityInStock(int quantityInStock) {
+        this.quantityInStock -= quantityInStock;
+    }
+    
+    public String infoToString() {
+        return "";
+    }
+    
+    public String toString() {
+        return "";
+    }
 }
