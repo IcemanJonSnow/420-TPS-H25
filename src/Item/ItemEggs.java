49e73a623d9e214e2f411e9288f421004e4a1e50
Samuel <parasam31@gmail.com>
2025-06-02 16:05:54 -0400

    parametres et methodes de la classe ItemEggs.java

--- src/Item/ItemEggs.java
+++ src/Item/ItemEggs.java
@@ -1,4 +1,31 @@
 package Item.Item;
 
-public class ItemEggs {
+public class ItemEggs extends Item {
+
+    private String color;
+    private int number;
+    
+    public ItemEggs(Category category, int id, String name, double price, String color, int number) {
+        super(category, id, name, price);
+        this.color = color;
+        this.number = number;
+    }
+    
+    public Category getCategory() {
+        return Category.Bread;
+    }
+    
+    public String getCategoryString(){
+        return "Bread";
+    }
+    
+    public String getColor() {return color;}
+    public void setColor(String color) {this.color = color;}
+    
+    public int getNumber() {return number;}
+    public void setNumber(int number) {this.number = number;}
+    
+    public String infoToString(){
+        return "";
+    }
 }
