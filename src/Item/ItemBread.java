fbbfda3e9f9984feaf62c53e516f8980f87dad61
Samuel <parasam31@gmail.com>
2025-06-02 16:01:31 -0400

    parametres et methodes de la classe ItemBread.java

--- src/Item/ItemBread.java
+++ src/Item/ItemBread.java
@@ -1,4 +1,31 @@
 package Item.Item;
 
-public class ItemBread {
+public class ItemBread extends Item {
+
+    private String color;
+    private double weight;
+    
+    public ItemBread(Category category, int id, String name, double price, String color, double weight) {
+        super(category, id, name, price);
+        this.color = color;
+        this.weight = weight;
+    }
+    
+    public Category getCategory() {
+        return Category.Bread;
+    }
+    
+    public String getCategoryString() {
+        return "Bread";
+    }
+    
+    public String getColor() {return color;}
+    public void setColor(String color) {this.color = color;}
+    
+    public double getWeight() {return weight;}
+    public void setWeight(double weight) {this.weight = weight;}
+    
+    public String infoToString() {
+        return "";
+    }
 }
