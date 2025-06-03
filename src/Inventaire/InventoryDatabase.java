e0cc90fc21537a9769ac781df7633ff8fc4f6afb
Samuel <parasam31@gmail.com>
2025-06-02 20:20:04 -0400

    Parametres et methode du package Inventaire

--- src/Inventaire/InventoryDatabase.java
+++ src/Inventaire/InventoryDatabase.java
@@ -1,4 +1,71 @@
 package Item.Inventaire;
 
+import Item.Item.Item;
+
 public class InventoryDatabase {
+
+    private int itemsCount = 0;
+    private InventoryDatabaseNode first;
+    
+    public void insert(Item item){
+        if (first == null){
+            InventoryDatabaseNode newItem = new InventoryDatabaseNode(item);
+            first = newItem;
+            itemsCount++;
+        }
+        else{
+            InventoryDatabaseNode newItem = new InventoryDatabaseNode(item);
+            InventoryDatabaseNode current = first;
+            while (current.next != null){
+                current = current.next;
+            }
+            current.next = newItem;
+            itemsCount++;
+            }
+    }
+    
+    public Item findById(int id){
+        if (first == null){
+            return null;
+        }
+        InventoryDatabaseNode current = first;
+        while (current.item.getId() != id){
+            current = current.next;
+            if (current == null){
+                return null;
+            }
+        }
+        return current.item;
+    }
+    
+    public void remove(int id){
+        if (first == null){
+            return;
+        }
+        if (findById(id) == null){
+            return;
+        }
+        else {
+            InventoryDatabaseNode current = first;
+            
+            while (current.next.item.getId() != id){
+                current = current.next;
+            }
+            current.next = current.next.next;
+            itemsCount--;
+        }
+    }
+    
+    
+    public Item[] getArrayOfitems(){
+        Item[] items = new Item[itemsCount];
+        for(int i = 0; i < itemsCount; i++){
+            InventoryDatabaseNode current = first;
+            while (current.next != null){
+                items[i] = current.item;
+                current = current.next;
+            }
+        }
+        return items;
+    }
 }
--- src/Inventaire/InventoryManager.java
+++ src/Inventaire/InventoryManager.java
@@ -1,4 +1,45 @@
 package Item.Inventaire;
 
+import Item.Item.*;
+
 public class InventoryManager {
+
+    private InventoryDatabase inventaire =  new InventoryDatabase();
+
+    public InventoryManager() {}
+
+    public void addNewBreadItem(int id, String name, double price, String color, int weight){
+        ItemBread newItem = new ItemBread(Category.Bread, id, name, price, color, weight);
+        inventaire.insert(newItem);
+    }
+
+    public void addNewEggsItem(int id, String name, double price, String color, int number){
+        ItemEggs newItem = new ItemEggs(Category.Eggs, id, name, price, color, number);
+        inventaire.insert(newItem);
+    }
+
+    public void addNewMilkItem(int id, String name, double price, double fat, double liters){
+        ItemMilk newItem = new ItemMilk(Category.Milk, id, name, price, fat, liters);
+        inventaire.insert(newItem);
+    }
+    
+    public void removeItem(int id){
+        inventaire.remove(id);
+    }
+    
+    public void increaseItemQuantity(int id, int quantity){
+        getItem(id).increaseQuantityInStock(quantity);
+    }
+    
+    public void decreaseItemQuantity(int id, int quantity){
+        getItem(id).decreaseQuantityInStock(quantity);
+    }
+    
+    public Item getItem(int id){
+        return inventaire.findById(id);
+    }
+    
+    public Item[] getArrayOfItems(){
+        return inventaire.getArrayOfitems();
+    }
 }
